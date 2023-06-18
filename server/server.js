1f3a15e7dd18ed41f62ebc7c81e45f8d489a6ca7
Bilal Shah <saybilalshah@gmail.com>
2023-06-18 14:24:19 +0800

    feat (server): handling broadcasting of the socket event

--- server/server.js
+++ server/server.js
@@ -6,11 +6,8 @@ const io = require("socket.io")(3001, {
 });
 
 io.on("connection", (socket) => {
-  console.log("a user connected");
-  socket.on("chat message", (msg) => {
-    console.log("message: " + msg);
-  });
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
+  socket.on("changes", (textChanges) => {
+    console.log("textChanges", textChanges);
+    socket.broadcast.emit("incomingChanges", textChanges);
   });
 });
